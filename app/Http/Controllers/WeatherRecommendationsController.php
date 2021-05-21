74e9e5026756614cff72b83c316ae8ad1b8767b8
Nemes1sX <kazstonk@gmail.com>
2021-05-21 22:06:39 +0300

    Cache has inplemented

--- app/Http/Controllers/WeatherRecommendationsController.php
+++ app/Http/Controllers/WeatherRecommendationsController.php
@@ -8,8 +8,9 @@ class WeatherRecommendationsController extends Controller
 {
     public function getWeatherRecommendations(string $city)
     {
-        $recommendations = (new WeatherRecommendationsService())->getRecommendation($city); //Gets recommendations from service class function
-
+        $recommendations = cache()->remember('recommendations', 60*5, function () use ($city)  {
+           return (new WeatherRecommendationsService())->getRecommendation($city); //Gets recommendations from service class function
+        });
         return response()->json(['city' => $city, 'recommendations' => $recommendations], 200);
     }
 }
