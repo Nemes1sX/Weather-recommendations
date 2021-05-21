7367819fc1e53bfeec611db8d7f5675fd67ac022
Nemes1sX <kazstonk@gmail.com>
2021-05-21 16:30:32 +0300

    Added comments

--- app/Http/Controllers/WeatherRecommendationsController.php
+++ app/Http/Controllers/WeatherRecommendationsController.php
@@ -8,7 +8,7 @@ class WeatherRecommendationsController extends Controller
 {
     public function getWeatherRecommendations(string $city)
     {
-        $recommendations = (new WeatherRecommendationsService())->getRecommendation($city);
+        $recommendations = (new WeatherRecommendationsService())->getRecommendation($city); //Gets recommendations from service class function
 
         return response()->json(['city' => $city, 'recommendations' => $recommendations], 200);
     }
--- app/Services/WeatherRecommendationsService.php
+++ app/Services/WeatherRecommendationsService.php
@@ -11,22 +11,22 @@ class WeatherRecommendationsService
 {
     public function getRecommendation(string $city)
     {
-        $startdate = Carbon::tomorrow();
-        $enddate = Carbon::tomorrow()->add(3, 'days');
+        $startdate = Carbon::tomorrow();  //Gets tommorow
+        $enddate = Carbon::tomorrow()->add(3, 'days'); //Gets 3 days ah of tommorrow
         $response = Http::get("https://api.meteo.lt/v1/places/{$city}/forecasts/long-term");
-        $forecasts = json_decode($response->body());
-        $i = 0;
+        $forecasts = json_decode($response->body()); //Getting the body of the forecast
+        $i = 0; //Initial index number of recommendations
         $recommendations = [];
         foreach ($forecasts->forecastTimestamps as $forecastTimestamp) {
             $time = Carbon::createFromFormat('Y-m-d H:i:s', $forecastTimestamp->forecastTimeUtc);
-            if ($time->hour == 12 && $time->between($startdate, $enddate)) {
+            if ($time->hour == 12 && $time->between($startdate, $enddate)) { //Checking if datetime belongs in that period and it's midday
                 $recommendations[$i]['weather_forecast'] = $forecastTimestamp->conditionCode;
                 $recommendations[$i]['date'] = $time->format('Y-m-d');
-                if (in_array($forecastTimestamp->conditionCode, ['clear', 'isolated-clouds', 'overcast', 'scattered-clouds'])) {
+                if (in_array($forecastTimestamp->conditionCode, ['clear', 'isolated-clouds', 'overcast', 'scattered-clouds'])) { //If weather is not rainy, it randomly will pick 2 products who fits for sunny weather
                     $products = Product::select('sku', 'name', 'price')->where('category', 'sunny')->take(2)
                         ->inRandomOrder()->get()->toArray();
                     $recommendations[$i]['products'] = array_values($products);
-                } elseif (in_array($forecastTimestamp->conditionCode, ['light-rain', 'moderate-rain', 'heavy-rain', 'sleet',
+                } elseif (in_array($forecastTimestamp->conditionCode, ['light-rain', 'moderate-rain', 'heavy-rain', 'sleet', //If weather is rainy or snowy, it randomly will pick 2 products who fits for sunny weather
                     'light-snow', 'moderate-snow', 'heavy-snow'])) {
                     $products = Product::select('sku', 'name', 'price')->where('category', 'rain')->take(2)
                         ->inRandomOrder()->get()->toArray();
